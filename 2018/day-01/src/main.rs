32e348c125726d4f22e3b7933a9a47e24778e6c6
Coty Rothery <cotyrothery@gmail.com>
2018-12-01 11:25:54 -0500

    Improved day 1 challenge

--- 2018/day-01/src/main.rs
+++ 2018/day-01/src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Error, ErrorKind};
 use std::collections::HashMap;
 
 fn main() {
+    let mut has_count_once: bool = false;
+    let mut first_count: i32 = 0;
     let mut count: i32 = 0;
     let mut hit_twice: i32 = 0;
     let mut has_hit_twice: bool = false;
@@ -17,27 +19,32 @@ fn main() {
             return
         }
     };
+    let file = File::open(&Path::new(&file_name)).unwrap();
+    let id_vals: Vec<i32> = match BufReader::new(file).lines().map(|line| 
+        line.and_then(|v| v.parse::<i32>().map_err(|e| Error::new(ErrorKind::InvalidData, e)))
+    ).collect() {
+        Ok(vals) => vals,
+        Err(e) => {
+            println!("{:?}", e);
+            vec![]
+        }
+    };
 
     loop {
-        let file = File::open(&Path::new(&file_name)).unwrap();
-
-        for line in BufReader::new(file).lines() {
-            let id_val: i32 = match line {
-                Ok(val) => val.parse::<i32>().unwrap(),
-                Err(e) => {
-                    println!("Failed to parse line: {:?}", e);
-                    0
-                }
-            };
-
+        for id_val in &id_vals {
             count += id_val;
             let hc = hit_count.entry(count).or_insert(0);
             *hc += 1;
             
             if !has_hit_twice && *hc > 1 {
                 has_hit_twice = true;
-                hit_twice = count.clone();
+                hit_twice = count;
             }
+        }
+
+        if !has_count_once {
+            has_count_once = true;
+            first_count = count;
         }
 
         if has_hit_twice {
@@ -45,6 +52,6 @@ fn main() {
         }
     }
 
-    println!("The count is {}!", count);
+    println!("The first count is {}!", first_count);
     println!("Hit numer {} twice first!", hit_twice);
 }
