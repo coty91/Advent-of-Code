00bc66f66d29a9de1fbf4af96648938d31aaf0ec
Coty Rothery <cotyrothery@gmail.com>
2018-12-01 10:03:33 -0500

    Day 1 part 2

--- 2018/day-01/src/main.rs
+++ 2018/day-01/src/main.rs
@@ -2,9 +2,13 @@ use std::env;
 use std::path::Path;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 
 fn main() {
     let mut count: i32 = 0;
+    let mut hit_twice: i32 = 0;
+    let mut has_hit_twice: bool = false;
+    let mut hit_count: HashMap<i32, i8> = HashMap::new(); 
     let args: Vec<String> = env::args().collect();
     let file_name: String = match args.get(1) {
         Some(val) => val.to_string(),
@@ -13,18 +17,36 @@ fn main() {
             return
         }
     };
-    let file = File::open(&Path::new(&file_name)).unwrap();
 
-    for line in BufReader::new(file).lines() {
-        let id_val: i32 = match line {
-            Ok(val) => val.parse::<i32>().unwrap(),
-            Err(e) => {
-                println!("Failed to parse line: {:?}", e);
-                0
+    loop {
+        let file = File::open(&Path::new(&file_name)).unwrap();
+
+        for line in BufReader::new(file).lines() {
+            let id_val: i32 = match line {
+                Ok(val) => val.parse::<i32>().unwrap(),
+                Err(e) => {
+                    println!("Failed to parse line: {:?}", e);
+                    0
+                }
+            };
+
+            count += id_val;
+            let hc = hit_count.entry(count).or_insert(0);
+            *hc += 1;
+
+            println!("{}: {}", count, *hc);
+            
+            if !has_hit_twice && *hc > 1 {
+                has_hit_twice = true;
+                hit_twice = count.clone();
             }
-        };
-        count += id_val;
+        }
+
+        if has_hit_twice {
+            break;
+        }
     }
 
     println!("The count is {}!", count);
+    println!("Hit numer {} twice first!", hit_twice);
 }
